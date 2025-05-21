200ececa183b96acd67149a12cce1ff9bbb99974
ryanpsouza <ryanpsouzagit@gmail.com>
2025-05-21 18:00:01 -0300

    Learning Django Logic

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("test/", views.index, name="index"),
-    path("add/", views.two, name="two")
+    path("index/", views.index, name="index"),
+    path("add/", views.two, name="two"),
+    path("<str:name>/", views.greet, name="greet")
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 def two(request):
     return HttpResponse("test")
+
+def greet(request, name):
+    return render(request, "hello/index.html", {
+        "name" : name.capitalize()
+    })
